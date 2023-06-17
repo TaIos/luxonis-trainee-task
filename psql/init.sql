87a44889533db443b1c3a5628de8b517af88afdd
talos <gismocz@gmail.com>
2023-06-17 14:52:19 +0200

    fix: table name

--- psql/init.sql
+++ psql/init.sql
@@ -1,19 +1,16 @@
 -- Create initial tables
 -- if they already exists, do nothing
 
-create table if not exists advertisement
+create table if not exists advertisements
 (
     id bigserial primary key,
-    title varchar(100) not null,
-    image varchar not null,
-    url varchar(100) not null,
+    "name" varchar(100) not null,
+    image_url varchar(100) not null,
+    scraped_hash_id varchar(100) not null
 );
 
-comment on table advertisement is 'Advertisements from sreality.cz – flats, sell';
-comment on column advertisement.id is 'Id of the advertisement.';
-comment on column advertisement.title is 'Title of the advertisement';
-comment on column advertisement.image is 'Image of the advertisement stored as base64';
-comment on column advertisement.url 'Url of the advertisement.';
-
--- TODO might add check for valid base64 advertisement.image here or in the application logic
--- TODO might add check for valid advertisement.url here or in the application logic
+comment on table advertisements is 'Advertisements from sreality.cz – flats, sell';
+comment on column advertisements.id is 'Id of the advertisement.';
+comment on column advertisements.name is 'Title of the advertisement';
+comment on column advertisements.image_url is 'Url of the advertised image';
+comment on column advertisements.scraped_hash_id is 'Hash id from the sreality assigned to the advertisement';
